ff9b253a77d21589d31c04f0bf7d07ec3dde2299
shimamura <46078198+shmn7iii@users.noreply.github.com>
2023-11-14 16:04:26 +0900

    fix: load dotenv

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,10 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 include Tapyrus::Opcodes
+Dotenv::Railtie.load
+
 module TapyrusIssuer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.

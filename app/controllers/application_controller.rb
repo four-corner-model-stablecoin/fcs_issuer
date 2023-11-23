0074e9033fbd24984863a437faa7a6f1df61dcf6
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-11-23 14:14:38 +0900

    feat: set default current_user to User.first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    @current_user ||= (User.find_by(id: session[:user_id]) if session[:user_id]) || User.first
   end
 
   def signed_in?
